1d4960f2347191522ffaca49018ee1508b32241e
Aamen <chen.yes.man@gmail.com>
2018-01-03 16:49:31 +0800

    create userlogin api

---
+++ application/controllers/user_login.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class user_login extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('home_page');
+	}
+}
